257667a5499578db70176dddfc8d57a3a38065ca
Maxim Zebzeev <maksimzeb@yandex.ru>
2021-05-02 22:21:07 +0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Введите свое имя: ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Привет {name}");
+        }
+    }
+}
